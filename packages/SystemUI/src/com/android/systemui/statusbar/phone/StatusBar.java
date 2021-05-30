8c7ba85985a43377dfc5dbccfa38915a570b8e84
Aryan Gupta <guptaaryan189@gmail.com>
2021-05-30 18:27:58 +0000

    fixup! dt2s on statusbar and ls gestures
    Revert "d2w: fix double tap to wake on ambient"
    
    This reverts commit 4604d841af31bb3815223d082a9888b5d2e7c29d.
    
    Revert "SystemUI: prevent that double tap to sleep triggers panel expand"
    
    This reverts commit 3846c8d26381430248d1929525ba1005cd7f9f81.
    
    Revert "base: Double tap to sleep on lockscreen [1/2]"
    
    This reverts commit 1ffc758ce677660872461098e798d30f5350dc93.
    
    Revert "[1/2] base: double tap to sleep on statusbar"
    
    This reverts commit a354b98862fad7259299d9327cc99ad5edb100c5.
    
    Double tap to sleep on lockscreen [1/2]
    
    aex edits:- Add a custom SettingsObserver
    
    Change-Id: Ice71af59a9fe09fa478318c113e2cfbd92764a00
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Double tap to sleep on statusbar [1/2]
    
    abc ezio84: use existing lockscreen doubletap gesture listener
    and abcutils screenoff action
    
    aex edits:- update for p
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -538,8 +538,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mUserSetup = userSetup;
                 if (!mUserSetup && mStatusBarView != null)
                     animateCollapseQuickSettings();
-                if (mNotificationPanelViewController != null) {
-                    mNotificationPanelViewController.setUserSetupComplete(mUserSetup);
+                if (mNotificationShadeWindowViewController != null) {
+                    mNotificationShadeWindowViewController.setLockscreenDoubleTapToSleep();
                 }
                 updateQsExpansionEnabled();
             }
@@ -2165,9 +2165,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_SHOW_BATTERY_PERCENT),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -2194,6 +2191,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL),
                     false, this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN),
+                    false, this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2214,6 +2217,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.SWITCH_STYLE))) {
                 stockSwitchStyle();
                 updateSwitchStyle();
+           } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
+                setLockscreenDoubleTapToSleep();
+           } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE))) {
+                setLockscreenDoubleTapToSleep();
             }
         }
 
@@ -2223,9 +2232,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             setQsBatteryPercentMode();
             setQsRowsColumns();
             setScreenBrightnessMode();
+            setLockscreenDoubleTapToSleep();
         }
     }
 
+   private void setLockscreenDoubleTapToSleep() {
+	    if (mNotificationShadeWindowViewController != null) {
+            mNotificationShadeWindowViewController.setLockscreenDoubleTapToSleep();
+        }
+    }
    private void setQsBatteryPercentMode() {
         if (mQSBarHeader != null) {
             ((QuickStatusBarHeader) mQSBarHeader).setBatteryPercentMode();
